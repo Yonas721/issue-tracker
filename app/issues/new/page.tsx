68dc73d412a31ca467722ebdd8d96456eba41cf9
Yonas <jemberuyonas01@gmail.com>
2026-02-27 22:55:59 +0300

    simple Mde for textarea editing is added

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,21 @@
 "use client";
 
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button,TextField } from "@radix-ui/themes";
+import dynamic from "next/dynamic";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+import "easymde/dist/easymde.min.css";
 
 export default function NewIssue() {
-
   return (
     <div className="space-y-3 max-w-xl">
       <TextField.Root placeholder="Title">
         <TextField.Slot></TextField.Slot>
       </TextField.Root>
-      <TextArea placeholder="Description" />
+      <SimpleMDE placeholder="Description"/>
       <Button>Submit new issue</Button>
     </div>
   );
-
 }
