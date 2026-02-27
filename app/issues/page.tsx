4b3d23c03e53acfb8e411117bf5ec601a21ac097
Yonas <jemberuyonas01@gmail.com>
2026-02-27 22:15:43 +0300

    radix-ui installed

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,6 @@
-export default function Issues(){
-    return (
-        <div>hello issues</div>
-    )
+import {  Button } from "@radix-ui/themes";
+
+export default function Issues() {
+  return <Button>hello issues</Button>;
+
 }
