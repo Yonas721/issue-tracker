000922043bb6ce3b2cd89b4c4d5088ca9aca282e
Yonas <jemberuyonas01@gmail.com>
2026-03-01 13:00:24 +0300

    viewing issue completed

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,34 +1,38 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/app/prisma";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
+import ReactMarkdown from "react-markdown";
 
-export default async function DetailsPage({params}:{params: Promise<{id:string}>}){
+export default async function DetailsPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
 
-const {id} = await params;
-
-const issue=await prisma.issue.findUnique({
+  const issue = await prisma.issue.findUnique({
     // we are parsing the id becuase it comes as a string
 
-    where: {id:parseInt(id)}
+    where: { id: parseInt(id) },
+  });
 
-});
-
-if (!issue){
-
+  if (!issue) {
     // this will triggert the rendering of the nearest not-found.tsx component
-    
     notFound();
-}
+  }
 
-return (
-<div>
-    <p>{issue.title}</p>
-    <p>{issue.description}</p>
-    <p>{issue.status}</p>
-    <p>{issue.created_at.toDateString()}</p>
-</div>
+  return (
+    <div>
+      <Heading>{issue.title}</Heading>
+      <Flex className="space-x-3 items-center my-4">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.created_at.toDateString()}</Text>
+      </Flex>
 
-);
-
-   
-
+      <Card className="prose">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
+    </div>
+  );
 }
