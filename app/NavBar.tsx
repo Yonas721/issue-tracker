e16d91f2194e6b06569e03bd741f98f90e4e4705
Yonas <jemberuyonas01@gmail.com>
2026-03-12 17:20:16 +0300

    small change on the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,53 +3,59 @@ import Link from "next/link";
 import { IoBugSharp } from "react-icons/io5";
 import { usePathname } from "next/navigation";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import {  Flex,Container } from "@radix-ui/themes";
 
 export default function NavBar() {
   const { status, data: session } = useSession();
   const currentPath = usePathname();
 
   return (
-    <nav className="flex space-x-6 border-b h-13 items-center mb-5 px-5 ">
-      <Link href="/" className="hover:text-red-500">
-        <IoBugSharp />
-      </Link>
-      <ul className="flex space-x-6">
-        <li>
-          <Link
-            href="/"
-            className={
-              currentPath === "/"
-                ? `text-gray-400`
-                : `hover:text-gray-300 transition-colors`
-            }
-          >
-            Dashboard
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/issues"
-            className={
-              currentPath === "/issues"
-                ? `text-gray-400`
-                : `hover:text-red-500 transition-colors`
-            }
-          >
-            Issues
-          </Link>
-        </li>
-      </ul>
+    <nav className="border-b h-13 items-center mb-5 px-5 py-5 ">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="4">
+            <Link href="/" className="hover:text-red-500">
+              <IoBugSharp />
+            </Link>
+            <ul className="flex space-x-6">
+              <li>
+                <Link
+                  href="/"
+                  className={
+                    currentPath === "/"
+                      ? `text-gray-400`
+                      : `hover:text-gray-300 transition-colors`
+                  }
+                >
+                  Dashboard
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/issues"
+                  className={
+                    currentPath === "/issues"
+                      ? `text-gray-400`
+                      : `hover:text-red-500 transition-colors`
+                  }
+                >
+                  Issues
+                </Link>
+              </li>
+            </ul>
+          </Flex>
 
-      <Box>
-        {status === "authenticated" && (
-          <Link href="/api/auth/signout">Logout</Link>
-        )}
+          <Flex>
+            {status === "authenticated" && (
+              <Link href="/api/auth/signout">Logout</Link>
+            )}
 
-        {status === "unauthenticated" && (
-          <Link href="/api/auth/signin">Login</Link>
-        )}
-      </Box>
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Login</Link>
+            )}
+          </Flex>
+        </Flex>
+      </Container>
     </nav>
   );
 }
