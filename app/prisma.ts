b57fe40703d863da23f8e6d89bb12603514b5203
Yonas <jemberuyonas01@gmail.com>
2026-03-19 00:14:00 +0300

    removed prisma-adapter package since neon doesnt need an adapter

--- app/prisma.ts
+++ app/prisma.ts
@@ -1,17 +1,8 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-
-const adapter = new PrismaMariaDb({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  connectionLimit: 5,
-});
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
