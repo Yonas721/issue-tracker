fe7162192d1e0b39a3a8cf269b6c741acdf4e829
Yonas <jemberuyonas01@gmail.com>
2026-03-19 23:53:44 +0300

    minor change

--- app/prisma.ts
+++ app/prisma.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
+    adapter: new PrismaNeon({ connectionString: process.env.DATABASE_URL! }),
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
